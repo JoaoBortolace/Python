d83239bef9340b85af9bf3141b900ccfb6d7bef2
João Bortolace <54319789+JoaoBortolace@users.noreply.github.com>
2023-02-06 16:25:15 -0300

    Create def01.py

---
+++ exercícios/tuplas/def01.py
@@ -0,0 +1,9 @@
+#Números por extenso
+numExt = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze' ,'quize', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+num = int(input('Digite um número entre 0 e 20: '))
+
+while ((num > 20) or (num < 0)):
+    num = int(input('Tente novamente. Digite um número entre 0 e 20: '))
+
+print(f'Você digitou o número {numExt[num]}!')
