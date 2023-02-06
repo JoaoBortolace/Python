d5a8f7a88cba59fd7a91c162a01427aec9b6cdb5
João Bortolace <54319789+JoaoBortolace@users.noreply.github.com>
2023-02-06 16:12:33 -0300

    Create tupla002.py

---
+++ aulas/tupla002.py
@@ -0,0 +1,9 @@
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+
+print(c)
+print(len(c))
+print(c.index(5, 2)) #Retorna a posição do parâmetro na tupla, o segundo parâmetro marca daonde a procura irá começar na tupla
+print(c.count(5)) #Retorna quantas vezes o parâmentro aparece na tupla
+print(sorted(c))
