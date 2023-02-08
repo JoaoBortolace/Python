1aebced9819a71d54fd7dc727e7364e118357056
João Bortolace <54319789+JoaoBortolace@users.noreply.github.com>
2023-02-08 12:32:47 -0300

    Update def78.py

--- exercícios/Listas/def78.py
+++ exercícios/Listas/def78.py
@@ -1,14 +1,15 @@
-# Leia 5 valores do teclado, guarde em uma lista, e exiba o maior e o menor e suas posições
-valores = list()
+valores = []
 for count in range(0, 5):
     valores.append(int(input(f'Digite um valor para a Posição {count}: ')))
-print(20*'-=-')
+print('-=' * 30)
 print(f'Você digitou os valores {valores}')
-print(f'O maior valor digitado foi {max(valores)} nas posições ', end='')
+maior = max(valores)
+print(f'O maior valor digitado foi {maior} nas posições ', end='')
 for pos, valor in enumerate(valores):
-    if valor == max(valores):
+    if valor == maior:
         print(f'{pos}... ', end='')
-print(f'\nO menor valor digitado foi {min(valores)} nas posições ', end='')
+menor = min(valores)
+print(f'\nO menor valor digitado foi {menor} nas posições ', end='')
 for pos, valor in enumerate(valores):
-    if valor == min(valores):
+    if valor == menor:
         print(f'{pos}... ', end='')
