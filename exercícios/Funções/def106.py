ea5345f8e38a5d93286fe2cb432ca8c6f0a578df
JoaoBortolace <54319789+JoaoBortolace@users.noreply.github.com>
2023-02-18 13:52:14 +0044

    pt2 exercício

--- exercícios/Funções/def106.py
+++ exercícios/Funções/def106.py
@@ -1,20 +1,34 @@
+from time import sleep as delay
 
 
-def ajuda(text):
-    return 0
+def ajuda(text=''):
+    header(f"Acessando o manual do comando '{text}'", cor='\033[44m')
+    delay(1)
+    print('\033[7m', end='')
+    help(text)
+    print('\033[m', end='')
+    delay(1)
 
 
-def header(text):
+def header(text, cor='\33[m'):
     tam = len(text) + 4
+    print(f'{cor}', end='')
     print('~' * tam)
     print(f'  {text}')
     print('~' * tam)
+    print('\033[m', end='')
 
 
 # Programa Principal
 while True:
-    header('SISTEMA DE AJUDA PyHELP')
+    header('SISTEMA DE AJUDA PyHELP', '\033[43m')
+    delay(.3)
     resp = str(input('Função ou Biblioteca > ')).strip()
-    if resp.lower() == '':
-        header('\033[0;36;41mATÉ LOGO!\033[m')
+    delay(.3)
+    if resp == '':
+        print('\033[31mERRO! Digite alguma Função/Biblioteca para obter ajuda ou escreva fim para sair.\033[m')
+    elif resp.lower() == 'fim':
+        header('ATÉ LOGO!', cor='\033[41m')
         break
+    else:
+        ajuda(resp)
